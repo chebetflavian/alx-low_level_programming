69083c802e8319f80e61ac46003967847f36ab65
chebetflavian <flavianchebet123@gmail.com>
2022-12-08 11:19:08 -0800

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,15 @@
 #!/bin/bash
 #include <stdio.h>
+
 /**
  *
- *  * main - Entry point
+ *  * main - Entry
  *
- *   *
+ *   * Return: Always 0
  *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ *    */
 int main(void)
 {
-
 		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);}
+			return (0);
+}
