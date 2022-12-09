eb81a229cdd9f1a1f23d3df80243dc5664f52e46
chebetflavian <flavianchebet123@gmail.com>
2022-12-09 03:39:33 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print numerbz
+ * Return: o
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
+
