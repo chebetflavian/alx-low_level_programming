7537826769d180aa991810b6e810c6f6633a44d8
chebetflavian <flavianchebet123@gmail.com>
2022-12-09 03:29:43 -0800

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print lowercase alphabet
+ * Return: 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+	putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
