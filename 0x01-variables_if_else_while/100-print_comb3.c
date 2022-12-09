66d7ceebf6c9de5b7dd2ddcfed5b87925ebe88f6
chebetflavian <flavianchebet123@gmail.com>
2022-12-09 03:50:47 -0800

    Combination of brains and materials

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - different combinations of two different numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x = 48; x <= 56; x++)
+	{
+		for (y = 49; y <= 57; y++)
+		{
+			if (y > x)
+			{
+				putchar(x);
+				putchar(y);
+				if (x != 56 || y != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
