f735e44b0b99c3e543472476dc822de5e76fa3e3
chebetflavian <flavianchebet123@gmail.com>
2022-12-13 03:16:24 -0800

    Insomnia alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *print_alphabet - print alphabet all in lowercse
+  */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	_putchar(letter);
+	_putchar('\n');
+}
